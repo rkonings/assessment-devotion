103662b0d5417b5f6219fa6e9f2a458bc17cf928
rkonings <41450820+rkonings@users.noreply.github.com>
2021-01-05 18:30:06 +0100

    feat(sidebar): add sidebar  (#2)
    * add focus trap hook package
    
    * add sidebar component
    
    * add story
    
    * add tests
    
    * add icons

--- src/Components/Layout/Sidebar.test.tsx
+++ src/Components/Layout/Sidebar.test.tsx
@@ -1,11 +1,29 @@
-import 'jest-styled-components';
 import React from 'react';
 import { mount } from 'enzyme';
-import Sidebar from './Sidebar';
+import Sidebar, { ClickAway, Overlay, SidebarBody, Close } from './Sidebar';
 
-describe('<Color />', () => {
-    test('render with given color', () => {
-        const wrapper = mount(<Sidebar />);
-        expect(wrapper).toHaveStyleRule('background', '#ffffff');
+describe('<Sidebar />', () => {
+    it('should render the sidebar', () => {
+        const wrapper = mount(<Sidebar isOpen={true}>FOO</Sidebar>);
+        expect(wrapper.find(ClickAway).length).toBe(1);
+        expect(wrapper.find(SidebarBody).length).toBe(1);
+        expect(wrapper.find(Overlay).length).toBe(1);
+    });
+
+    it('should remove the sidebar on outside click', () => {
+        const wrapper = mount(<Sidebar isOpen={true}>FOO</Sidebar>);
+        wrapper.find(ClickAway).simulate('click');
+        expect(wrapper.find(SidebarBody).length).toBe(0);
+    });
+
+    it('should remove the sidebar when mouseclick on close icon', () => {
+        const wrapper = mount(<Sidebar isOpen={true}>FOO</Sidebar>);
+        wrapper.find(Close).simulate('click');
+        expect(wrapper.find(SidebarBody).length).toBe(0);
+    });
+
+    it('should render children in the body', () => {
+        const wrapper = mount(<Sidebar isOpen={true}>FOO</Sidebar>);
+        expect(wrapper.find(SidebarBody).text()).toEqual('FOO');
     });
 });
