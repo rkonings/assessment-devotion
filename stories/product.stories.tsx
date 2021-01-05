b8b2ef200b895f8461eff8e67ab568324b839b17
rkonings <randy@randykonings.nl>
2021-01-05 17:51:42 +0100

    add product overview story

--- stories/product.stories.tsx
+++ stories/product.stories.tsx
@@ -3,24 +3,58 @@ import { text, boolean, number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import React from 'react';
 import ProductTile from '../src/Components/Product/ProductTile';
+import ProductOverview from '../src/Components/Product/ProductOverview';
 
-storiesOf('Product', module).add('product tile', () => {
-    const onWishList = boolean('OnWishtList', false);
-    const id = 1;
-    const title = text('title', 'DOO');
-    const category = text('category', 'FOO');
-    const price = number('price', 100.0);
-    const description = 'FOOBAZ';
-    const image = text(
-        'image',
-        'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'
-    );
+storiesOf('Product', module)
+    .add('product tile', () => {
+        const onWishList = boolean('OnWishtList', false);
+        const id = 1;
+        const title = text('title', 'DOO');
+        const category = text('category', 'FOO');
+        const price = number('price', 100.0);
+        const description = 'FOOBAZ';
+        const image = text(
+            'image',
+            'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'
+        );
 
-    return (
-        <ProductTile
-            onWishList={onWishList}
-            toggleWishList={action('toggle')}
-            product={{ id, title, category, price, image, description }}
-        />
-    );
-});
+        return (
+            <ProductTile
+                onWishList={onWishList}
+                toggleWishList={action('toggle')}
+                product={{ id, title, category, price, image, description }}
+            />
+        );
+    })
+    .add('product overview', () => {
+        const id = 1;
+        const title = text('title', 'DOO');
+        const category = text('category', 'FOO');
+        const price = number('price', 100.0);
+        const description = 'FOOBAZ';
+        const image = text(
+            'image',
+            'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'
+        );
+
+        return (
+            <ProductOverview>
+                {Array(12)
+                    .fill(12)
+                    .map(() => (
+                        <ProductTile
+                            onWishList={Math.random() >= 0.5}
+                            toggleWishList={action('toggle')}
+                            product={{
+                                id,
+                                title,
+                                category,
+                                price,
+                                image,
+                                description,
+                            }}
+                        />
+                    ))}
+            </ProductOverview>
+        );
+    });
