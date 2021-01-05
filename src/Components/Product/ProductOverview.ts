aedea190f7fe82ecc8b9108e22f3ef1d2e8ae257
rkonings <randy@randykonings.nl>
2021-01-05 17:51:32 +0100

    add styling to product overview

--- src/Components/Product/ProductOverview.ts
+++ src/Components/Product/ProductOverview.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export default styled.div``;
+export default styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-wrap: wrap;
+    align-items: flex-start;
+`;
