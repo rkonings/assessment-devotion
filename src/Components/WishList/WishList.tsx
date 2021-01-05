7dd676b98e9abbdaafcba74e46954f2823706791
rkonings <randy@randykonings.nl>
2021-01-05 20:20:09 +0100

    add wishlist component

---
+++ src/Components/WishList/WishList.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import Sidebar from '../Layout/Sidebar';
+import { Product } from '../Product/interfaces';
+import WishListItem from './WishListItem';
+
+interface WishList {
+    wishListItems: Map<number | string, number>;
+    products: Product[];
+    onChange: (items: Map<number | string, number>) => void;
+}
+
+const getWishListItemProduct = (id: number | string, products: Product[]) => {
+    const product = products.find((product) => product.id == id);
+    return product;
+};
+
+const List = styled.div`
+    padding: 2em 1em;
+`;
+
+export default ({ wishListItems, products, onChange }: WishList) => {
+    const listItems: JSX.Element[] = [];
+
+    wishListItems.forEach((value, key) => {
+        const product = getWishListItemProduct(key, products);
+        if (product) {
+            listItems.push(
+                <WishListItem
+                    price={product.price}
+                    removeHandler={() => {
+                        const changedItems = new Map<number | string, number>(
+                            wishListItems
+                        );
+                        changedItems.delete(key);
+                        onChange(changedItems);
+                    }}
+                    category={product.category}
+                    changeAmount={(amount) => {
+                        const changedItems = new Map<number | string, number>(
+                            wishListItems
+                        );
+                        changedItems.set(key, amount);
+                        onChange(changedItems);
+                    }}
+                    amount={value}
+                    title={product.title}
+                    image={product.image}
+                />
+            );
+        }
+    });
+
+    return (
+        <Sidebar isOpen={true}>
+            <List>{listItems}</List>
+        </Sidebar>
+    );
+};
