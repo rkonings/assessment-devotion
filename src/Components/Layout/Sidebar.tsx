7f8256f8dd9e21d59f6c6f14bc64ce69fcc70bbc
rkonings <randy@randykonings.nl>
2021-01-05 18:29:08 +0100

    add sidebar component

--- src/Components/Layout/Sidebar.tsx
+++ src/Components/Layout/Sidebar.tsx
@@ -1,18 +1,76 @@
 import React from 'react';
 
 import styled from 'styled-components';
+import useFocusTrap from '@charlietango/use-focus-trap';
+import { Close as CloseIcon } from '../Icons';
+
+export const ClickAway = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 1;
+`;
+
+export const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 1;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
+    background: rgba(0, 0, 0, 0.4);
+    z-index: 999;
+`;
+
+export const Close = styled.div`
+    position: absolute;
+    right: 10px;
+    top: 10px;
+`;
 
 interface Sidebar {
     className?: string;
+    isOpen?: boolean;
+    children: string | JSX.Element | JSX.Element[];
 }
 
-export default styled(({ className }: Sidebar) => {
-    return <div className={className}>Sidebar</div>;
-})`
-    width: 300px;
-    position: abolute;
-    right: 0;
-    top: 0;
-    bottom: 0;
+export const SidebarBody = styled.div`
+    width: 400px;
     height: 100%;
+    z-index: 1;
+    display: flex;
+    flex-direction: column;
+    background: #ffffff;
 `;
+
+const Sidebar = ({ className, isOpen, children }: Sidebar) => {
+    const [open, setOpen] = React.useState<boolean>();
+    const focusTrap = useFocusTrap();
+
+    React.useEffect(() => {
+        setOpen(isOpen);
+    }, [isOpen]);
+
+    return (
+        <div className={className}>
+            {open && (
+                <Overlay>
+                    <ClickAway onClick={() => setOpen(false)} />
+                    <SidebarBody ref={focusTrap}>
+                        {children}
+                        <Close onClick={() => setOpen(false)}>
+                            <CloseIcon />
+                        </Close>
+                    </SidebarBody>
+                </Overlay>
+            )}
+        </div>
+    );
+};
+
+export default styled(Sidebar)``;
