9ee1a4ae327c644068642e7c66fb459ed1da89c6
rkonings <randy@randykonings.nl>
2021-01-05 18:29:17 +0100

    add story

--- stories/sidebar.stories.tsx
+++ stories/sidebar.stories.tsx
@@ -1,7 +1,8 @@
 import { storiesOf } from '@storybook/react';
+import { boolean } from '@storybook/addon-knobs';
 import React from 'react';
 import Sidebar from '../src/Components/Layout/Sidebar';
 
 storiesOf('Sidebar', module).add('default', () => {
-    return <Sidebar />;
+    return <Sidebar isOpen={boolean('open', true)}>FOO</Sidebar>;
 });
