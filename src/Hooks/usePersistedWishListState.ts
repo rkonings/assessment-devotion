544575eedee0efd9299545bbf9d25f422fcf04d3
rkonings <randy@randykonings.nl>
2021-01-06 13:30:41 +0100

    fix return typings

--- src/Hooks/usePersistedWishListState.ts
+++ src/Hooks/usePersistedWishListState.ts
@@ -1,8 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 
 const STORAGE_KEY = 'wishlist';
 
-export default () => {
+export default (): [
+    Map<string | number, number>,
+    Dispatch<SetStateAction<Map<string | number, number>>>
+] => {
     const rawStoredItems = localStorage.getItem(STORAGE_KEY);
     const storedItems = rawStoredItems ? JSON.parse(rawStoredItems) : [];
 
