c38ca76197cd86531e682255b919017916da2c8e
rkonings <41450820+rkonings@users.noreply.github.com>
2021-01-06 13:55:17 +0100

    fix(typescript): ts errors (#15)
    * fix return typings
    
    * remove unnecessary typing
    
    * fix tests with close handler mock function

--- src/Components/Layout/Sidebar.test.tsx
+++ src/Components/Layout/Sidebar.test.tsx
@@ -3,27 +3,49 @@ import { mount } from 'enzyme';
 import Sidebar, { ClickAway, Overlay, SidebarBody, Close } from './Sidebar';
 
 describe('<Sidebar />', () => {
+    const closeHandlerMock = jest.fn();
+
     it('should render the sidebar', () => {
-        const wrapper = mount(<Sidebar isOpen={true}>FOO</Sidebar>);
+        const wrapper = mount(
+            <Sidebar closeHandler={closeHandlerMock} isOpen={true}>
+                FOO
+            </Sidebar>
+        );
         expect(wrapper.find(ClickAway).length).toBe(1);
         expect(wrapper.find(SidebarBody).length).toBe(1);
         expect(wrapper.find(Overlay).length).toBe(1);
     });
 
     it('should remove the sidebar on outside click', () => {
-        const wrapper = mount(<Sidebar isOpen={true}>FOO</Sidebar>);
+        const wrapper = mount(
+            <Sidebar closeHandler={closeHandlerMock} isOpen={true}>
+                FOO
+            </Sidebar>
+        );
         wrapper.find(ClickAway).simulate('click');
-        expect(wrapper.find(SidebarBody).length).toBe(0);
+        expect(closeHandlerMock).toBeCalledTimes(1);
     });
 
     it('should remove the sidebar when mouseclick on close icon', () => {
-        const wrapper = mount(<Sidebar isOpen={true}>FOO</Sidebar>);
+        const wrapper = mount(
+            <Sidebar closeHandler={closeHandlerMock} isOpen={true}>
+                FOO
+            </Sidebar>
+        );
         wrapper.find(Close).simulate('click');
-        expect(wrapper.find(SidebarBody).length).toBe(0);
+        expect(closeHandlerMock).toBeCalledTimes(1);
     });
 
     it('should render children in the body', () => {
-        const wrapper = mount(<Sidebar isOpen={true}>FOO</Sidebar>);
+        const wrapper = mount(
+            <Sidebar closeHandler={closeHandlerMock} isOpen={true}>
+                FOO
+            </Sidebar>
+        );
         expect(wrapper.find(SidebarBody).text()).toEqual('FOO');
+    });
+
+    afterEach(() => {
+        closeHandlerMock.mockReset();
     });
 });
