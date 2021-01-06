2a1bfc70f3d4a4c4b95fc68cf18bb3dfdfafb371
rkonings <randy@randykonings.nl>
2021-01-06 14:01:33 +0100

    add top margin on mobile

--- src/Components/Layout/Page.ts
+++ src/Components/Layout/Page.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 export default styled.div`
     width: 100%;
+    margin-top: 100px;
     @media screen and (min-width: 768px) {
         max-width: 1000px;
         margin: 100px auto 0 auto;
