9b49ae5bbf2fe0afba9c05d397f24fbb3ea540b1
rkonings <randy@randykonings.nl>
2021-01-06 13:25:39 +0100

    make element responsive

--- src/Components/Layout/Page.ts
+++ src/Components/Layout/Page.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 export default styled.div`
-    width: 1200px;
-    margin: 100px auto 0 auto;
+    width: 100%;
+    @media screen and (min-width: 768px) {
+        max-width: 1000px;
+        margin: 100px auto 0 auto;
+    }
 `;
